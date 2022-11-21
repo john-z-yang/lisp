ae33b5c0715d9af71cca7bb3a3efa7099d2dda94
John Yang <johnyang@protonmail.com>
2022-11-20 16:49:05 -0800

    Adds missing namespace specifier

--- src/sexpr/cast.cpp
+++ src/sexpr/cast.cpp
@@ -9,7 +9,7 @@ template <typename To, typename From> bool isa(From &f) {
 template <typename To, typename From>
 std::shared_ptr<To> cast(std::shared_ptr<From> f) {
   if (isa<To>(*f)) {
-    return dynamic_pointer_cast<To>(f);
+    return std::dynamic_pointer_cast<To>(f);
   }
   std::stringstream ss;
   ss << "Mismatched types. Expected \"" << To::typeName << "\", but got \""
