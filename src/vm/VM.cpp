a52765ebdf5942389118ccb7d068e30ecb6cb793
John Yang <johnyang@protonmail.com>
2023-04-19 00:24:04 -0700

    Implement computed GOTO

--- src/vm/VM.cpp
+++ src/vm/VM.cpp
@@ -1,5 +1,4 @@
 #include "VM.hpp"
-#include "../code/OpCode.hpp"
 #include "../sexpr/BoolAtom.hpp"
 #include "../sexpr/ClosureAtom.hpp"
 #include "../sexpr/NatFnAtom.hpp"
@@ -13,120 +12,142 @@
 VM::VM() {}
 
 std::shared_ptr<SExpr> VM::exec(std::shared_ptr<FnAtom> main) {
+#define CUR_FRAME() (frames.back())
+#define CUR_CLOSURE() (CUR_FRAME().closure)
+#define CUR_FN() (CUR_CLOSURE()->fnAtom)
+#define CUR_CODE() (CUR_FN()->code)
+#define BASE_PTR() (CUR_FRAME().bp)
+#define INST_PTR() (CUR_FRAME().ip)
+
+#define READ_BYTE() (CUR_CODE().byteCodes[INST_PTR()++])
+#define READ_SHORT()                                                           \
+  (INST_PTR() += 2, (uint16_t)((CUR_CODE().byteCodes[INST_PTR() - 2] << 8 |    \
+                                CUR_CODE().byteCodes[INST_PTR() - 1])))
+#define READ_CONST() (CUR_CODE().consts[READ_BYTE()])
+
+#define DISPATCH() goto *dispatchTable[READ_BYTE()]
+
+  static void *dispatchTable[] = {&&MAKE_CLOSURE, &&CALL,
+                                  &&RETURN,       &&POP_TOP,
+                                  &&LOAD_CONST,   &&LOAD_SYM,
+                                  &&DEF_SYM,      &&SET_SYM,
+                                  &&LOAD_UPVALUE, &&SET_UPVALUE,
+                                  &&LOAD_STACK,   &&SET_STACK,
+                                  &&JUMP,         &&POP_JUMP_IF_FALSE,
+                                  &&MAKE_VAR_ARGS};
+
   stack.push_back(std::make_shared<ClosureAtom>(main));
   call(0);
 
-#define READ_BYTE() (curFrame.closure->fnAtom->code.byteCodes[curFrame.ip++])
-#define READ_SHORT()                                                           \
-  (curFrame.ip += 2,                                                           \
-   (uint16_t)((curFrame.closure->fnAtom->code.byteCodes[curFrame.ip - 2]       \
-                   << 8 |                                                      \
-               curFrame.closure->fnAtom->code.byteCodes[curFrame.ip - 1])))
-#define READ_CONST() (curFrame.closure->fnAtom->code.consts[READ_BYTE()])
+  DISPATCH();
 
-  while (true) {
-    auto &curFrame = frames.back();
-    uint8_t byte = READ_BYTE();
-    switch (byte) {
-    case OpCode::RETURN: {
-      auto res = stack.back();
-      if (frames.size() == 1) {
-        frames.clear();
-        stack.clear();
-        return res;
-      }
-      while (stack.size() > frames.back().bp) {
-        stack.pop_back();
-      }
-      frames.pop_back();
-      stack.push_back(res);
-      break;
-    }
-    case OpCode::CALL: {
-      const auto argc = READ_BYTE();
-      call(argc);
-      break;
-    }
-    case OpCode::MAKE_CLOSURE: {
-      auto closure = std::make_shared<ClosureAtom>(cast<FnAtom>(READ_CONST()));
-      for (unsigned int i{0}; i < closure->fnAtom->numUpVals; ++i) {
-        auto isLocal = READ_BYTE();
-        auto idx = READ_BYTE();
-        if (isLocal == 1) {
-          closure->upValues.push_back(stack[curFrame.bp + idx]);
-        } else {
-          closure->upValues.push_back(curFrame.closure->upValues[idx]);
-        }
-      }
-      stack.push_back(closure);
-      break;
-    }
-    case OpCode::POP_TOP: {
-      stack.pop_back();
-      break;
-    }
-    case OpCode::LOAD_CONST: {
-      stack.push_back(READ_CONST());
-      break;
-    }
-    case OpCode::LOAD_SYM: {
-      stack.push_back(globals.find(*cast<SymAtom>(READ_CONST())));
-      break;
-    }
-    case OpCode::DEF_SYM: {
-      globals.def(*cast<SymAtom>(READ_CONST()), stack.back());
-      break;
-    }
-    case OpCode::SET_SYM: {
-      globals.set(*cast<SymAtom>(READ_CONST()), stack.back());
-      break;
-    }
-    case OpCode::LOAD_UPVALUE: {
-      stack.push_back(curFrame.closure->upValues[READ_BYTE()]);
-      break;
-    }
-    case OpCode::SET_UPVALUE: {
-      curFrame.closure->upValues[READ_BYTE()] = stack.back();
-      break;
-    }
-    case OpCode::LOAD_STACK: {
-      stack.push_back(stack[curFrame.bp + READ_BYTE()]);
-      break;
-    }
-    case OpCode::JUMP: {
-      curFrame.ip += READ_SHORT();
-      break;
-    }
-    case OpCode::POP_JUMP_IF_FALSE: {
-      auto offset = READ_SHORT();
-      if (!BoolAtom::toBool(stack.back())) {
-        curFrame.ip += offset;
-      }
-      stack.pop_back();
-      break;
-    }
-    case OpCode::MAKE_VAR_ARGS: {
-      const auto n = stack.size() - curFrame.bp - 1;
-      if (n == 0) {
-        stack.push_back(std::make_shared<NilAtom>());
-        break;
-      }
-      const auto list = makeList(n);
-      for (std::vector<std::shared_ptr<SExpr>>::size_type i{0}; i < n; ++i) {
-        stack.pop_back();
-      }
-      stack.push_back(list);
-      break;
-    }
-    default:
-      break;
+MAKE_CLOSURE : {
+  const auto closure =
+      std::make_shared<ClosureAtom>(cast<FnAtom>(READ_CONST()));
+  for (unsigned int i{0}; i < closure->fnAtom->numUpVals; ++i) {
+    auto isLocal = READ_BYTE();
+    auto idx = READ_BYTE();
+    if (isLocal == 1) {
+      closure->upValues.push_back(stack[BASE_PTR() + idx]);
+    } else {
+      closure->upValues.push_back(CUR_CLOSURE()->upValues[idx]);
     }
   }
-  return stack.back();
+  stack.push_back(closure);
+}
+  DISPATCH();
+CALL : {
+  call(READ_BYTE());
+  DISPATCH();
+}
+RETURN : {
+  auto res = stack.back();
+  if (frames.size() == 1) {
+    frames.clear();
+    stack.clear();
+    return res;
+  }
+  while (stack.size() > frames.back().bp) {
+    stack.pop_back();
+  }
+  frames.pop_back();
+  stack.push_back(res);
+}
+  DISPATCH();
+POP_TOP : {
+  stack.pop_back();
+  DISPATCH();
+}
+LOAD_CONST : {
+  stack.push_back(READ_CONST());
+  DISPATCH();
+}
+LOAD_SYM : {
+  stack.push_back(globals.find(*cast<SymAtom>(READ_CONST())));
+  DISPATCH();
+}
+DEF_SYM : {
+  globals.def(*cast<SymAtom>(READ_CONST()), stack.back());
+  DISPATCH();
+}
+SET_SYM : {
+  globals.set(*cast<SymAtom>(READ_CONST()), stack.back());
+  DISPATCH();
+}
+LOAD_UPVALUE : {
+  stack.push_back(CUR_CLOSURE()->upValues[READ_BYTE()]);
+  DISPATCH();
+}
+SET_UPVALUE : {
+  CUR_CLOSURE()->upValues[READ_BYTE()] = stack.back();
+  DISPATCH();
+}
+LOAD_STACK : {
+  stack.push_back(stack[BASE_PTR() + READ_BYTE()]);
+  DISPATCH();
+}
+SET_STACK : {
+  stack[BASE_PTR() + READ_BYTE()] = stack.back();
+  DISPATCH();
+}
+JUMP : {
+  INST_PTR() += READ_SHORT();
+  DISPATCH();
+}
+POP_JUMP_IF_FALSE : {
+  auto offset = READ_SHORT();
+  if (!BoolAtom::toBool(stack.back())) {
+    INST_PTR() += offset;
+  }
+  stack.pop_back();
+  DISPATCH();
+}
+MAKE_VAR_ARGS : {
+  const auto n = stack.size() - BASE_PTR() - 1;
+  if (n == 0) {
+    stack.push_back(std::make_shared<NilAtom>());
+  } else {
+    const auto list = makeList(n);
+    for (std::vector<std::shared_ptr<SExpr>>::size_type i{0}; i < n; ++i) {
+      stack.pop_back();
+    }
+    stack.push_back(list);
+  }
+  DISPATCH();
+}
+
+#undef CUR_FRAME
+#undef CUR_CLOSURE
+#undef CUR_FN
+#undef CUR_CODE
+#undef BASE_PTR
+#undef INST_PTR
 
 #undef READ_BYTE
 #undef READ_SHORT
 #undef READ_CONST
+
+#undef DISPATCH
 }
 
 void VM::call(const uint8_t argc) {
