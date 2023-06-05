8f85d979f23ba786fdca69e4a7e1b0222a94f2ad
John Yang <johnyang@protonmail.com>
2023-06-04 21:35:04 -0700

    Add else clause and error reporting to cond

--- lib/logic.lisp
+++ lib/logic.lisp
@@ -19,9 +19,23 @@
 
 (defmacro cond sexprs
   (if (null? sexprs) '()
-    (list 'if
-          (car (car sexprs))
-          (if (null? (cdr (car sexprs)))
-            (error "Missing expression after test")
-            (cdr (car sexprs)))
-          (cons 'cond (cdr sexprs)))))
+    ((lambda (cur-clause next-clause)
+      (if (not (cons? cur-clause))
+        (error (str-con "Invalid syntax for cond. "
+                        "Expected (test conseq), but got "
+                        (->str cur-clause)
+                        "."))
+        (list 'if
+              (if (eqv? (car cur-clause) 'else)
+                (if (null? next-clause)
+                  #t
+                  (error (str-con "Invalid syntax for cond "
+                                  "(else clause must be last).")))
+                (car cur-clause))
+              (if (null? (cdr cur-clause))
+                (error (str-con "Invalid syntax for cond "
+                                "(missing expression after test)."))
+                (car (cdr cur-clause)))
+              (cons 'cond next-clause))))
+    (car sexprs)
+    (cdr sexprs))))
