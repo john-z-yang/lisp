e3b67c1a391ac557dfe3d0d0c167cec90fecc66e
John Yang <johnyang@protonmail.com>
2023-06-23 22:07:30 -0700

    Add missing header

--- src/code/InstrPtr.hpp
+++ src/code/InstrPtr.hpp
@@ -1,8 +1,8 @@
 #ifndef LISP_SRC_CODE_INSTRPTR_HPP_
 #define LISP_SRC_CODE_INSTRPTR_HPP_
 
+#include <cstdint>
 #include <vector>
-
 namespace code {
 
 using InstrPtr = std::vector<uint8_t>::size_type;
